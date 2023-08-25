4b75dbdbe08d141f168014c6019424626c0ef693
Igorandrade07 <136855017+Igorandrade07@users.noreply.github.com>
2023-08-25 18:15:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const wrapper = document.querySelector(".wrapper"),
-    qrinput = wrapper.querySelector(".form input"),
-    generateBtn = wrapper.querySelector(".form button"),
-    qrImg = wrapper.querySelector(".qr-code img");
-let prevalue;
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
 
 generateBtn.addEventListener("click", () => {
-    let qrvalue = qrinput.value.trim();
-    if (!qrvalue || prevalue === qrvalue) return;
-    prevalue = qrvalue;
-    generateBtn.innertext = "generating QR Code...";
-    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}';
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
-        generateBtn.innertext = "generate QR Code";
-    })
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
 
-})
-qrinput.addEventListener("keyup", () => {
-    if (!qrinput.value.trim()) {
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
         wrapper.classList.remove("active");
-        prevalue = "";
+        preValue = "";
     }
 });
